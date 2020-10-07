4d49068f1291cb23780d40d9614ca3bd93bd53a4
Ben8842 <z87e99b44r55a@gmail.com>
2020-10-07 18:09:11 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from "react";
+import ItemsContext from "./context/items-context";
+import itemsReducer from "./reducers/items";
+import AddItemForm from "./components/AddItemForm";
+import "./App.css";
+import ItemList from "./components/ItemList";
 
 function App() {
+  const [items, itemsDispatch] = useReducer(itemsReducer, []);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("items"));
+    if (items) {
+      itemsDispatch({ type: "POPULATE_ITEMS", items });
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ItemsContext.Provider value={{ items, itemsDispatch }}>
+      <div className="App">
+        <header className="App-header">
+          To Do items
+          <ItemList />
+          <AddItemForm />
+        </header>
+      </div>
+    </ItemsContext.Provider>
   );
 }
 
